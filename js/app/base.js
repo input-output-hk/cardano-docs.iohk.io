365de712eb98320edd227b9d92ad9c1ca77919c7
Tomas Vrana <tomas@vrana.org>
2017-01-25 12:32:26 +0000

    search module

--- js/app/base.js
+++ js/app/base.js
@@ -4,9 +4,6 @@ var chosen = '';
 var io_hist = new Object();
 var svgloader = '<div class="text-center" style="padding:50px 0"><?xml version="1.0" encoding="utf-8"?><svg width="120px" height="120px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-ring-alt"><rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect><circle cx="50" cy="50" r="40" stroke="#333333" fill="none" stroke-width="10" stroke-linecap="round"></circle><circle cx="50" cy="50" r="40" stroke="#ff0000" fill="none" stroke-width="6" stroke-linecap="round"><animate attributeName="stroke-dashoffset" dur="2s" repeatCount="indefinite" from="0" to="502"></animate><animate attributeName="stroke-dasharray" dur="2s" repeatCount="indefinite" values="150.6 100.4;1 250;150.6 100.4"></animate></circle></svg></div>';
 
-if(lowerie){
-	document.documentElement.innerHTML = 'Update your browser please!<br><a href="http://outdatedbrowser.com" target="_blank">Check available browsers</a>';
-}
 
 var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
 var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
